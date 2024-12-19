065c4cba65f2a0415744a46f32a80aa5bcfcfb35
Fahrurroziy <148849905+fahrurroziy80@users.noreply.github.com>
2024-12-19 07:54:05 +0700

    Create Pengulangan.py
    Pengulangan dan logika dasar dalam pemrograman

---
+++ Pengulangan.py
@@ -0,0 +1,19 @@
+def tampil():
+    
+    angka = 20
+    
+    for angka in range(angka):
+        
+        angka < 10
+    
+    print(angka)
+
+tampil()
+
+def la():
+    
+    angka = 1
+    while angka >11:
+        print(angka)
+    angka += 2
+la()
